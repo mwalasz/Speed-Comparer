6d3c8d00cf72af17f112068ea372dbca452cedf1
mwalasz <mwalasz@interia.pl>
2019-10-21 17:12:32 +0200

    Usunięcie niepotrzebnych plików

--- Libs/AssemblyDll/AssemblyDll.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include <windows.h> 
-
-extern "C" int _stdcall MyProc1(DWORD x, DWORD y);
-extern "C" int _stdcall ReadTime_1(void);
--- MainProgram/Utils/AssemblyDll.cs
+++ MainProgram/Utils/AssemblyDll.cs
@@ -1,11 +1,11 @@
 ﻿using System.Runtime.InteropServices;
 
 namespace MainProgram.Utils
-{ 
+{
     public static class AssemblyDll
     {
-        private const string PathToDll = @"D:\Studia\programowanie\AssemblyProject\Debug\AssemblyDll.dll";
-        
+        private const string PathToDll = @"D:\Studia\programowanie\Speed-Comparer\Libs\AssemblyDll\Debug\AssemblyDll.dll";
+
         [DllImport(PathToDll)]
         public static extern int AsmVal();
     }
