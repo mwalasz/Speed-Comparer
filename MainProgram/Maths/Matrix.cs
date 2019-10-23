05b2c7ffc2af2a29733f07930053052cdd23e59c
mwalasz <mwalasz@interia.pl>
2019-10-23 19:37:02 +0200

    Przeniesienie klasy matrix do głównego projektu, dodanie klasy reprezentującej skalar

---
+++ MainProgram/Maths/Matrix.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Linq;
+
+namespace MainProgram.Maths
+{
+    public class Matrix
+    {
+        private readonly int[,] content;
+
+        public int Columns { get; set; }
+        public int Rows { get; set; }
+
+        public Matrix(int rows, int columns)
+        {
+            Rows = rows;
+            Columns = columns;
+
+            content = new int[Rows, Columns];
+        }
+
+        public Matrix(int[,] matrix)
+        {
+            Rows = matrix.GetLength(0);
+            Columns = matrix.GetLength(1);
+
+            content = matrix;
+        }
+
+        public Matrix MultiplyByScalar(Scalar scalar)
+        {
+            for (int i = 0; i < Rows; i++)
+            {
+                for (int j = 0; j < Columns; j++)
+                {
+                    content[i, j] = content[i, j] * scalar.Value;
+                }
+            }
+
+            return new Matrix(content);
+        }
+
+        public override string ToString()
+        {
+            var numbers = content.Cast<int>()
+                .Select(i => i.ToString())
+                .ToArray();
+
+            string toDisplay = string.Empty;
+            int start = 0;
+            
+            for (int i = 0; i < Rows; i++)
+            {
+                var line = string.Join(' ', numbers, start, Columns);
+                toDisplay += (line + Environment.NewLine);
+
+                start += Columns;
+            }
+
+            return toDisplay;
+        }
+    }
+}
---
+++ MainProgram/Maths/Scalar.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MainProgram.Maths
+{
+    public class Scalar
+    {
+        public int Value { get; set; }
+        
+        public Scalar(int value)
+        {
+            Value = value;
+        }
+
+        public Scalar(string value)
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                throw new System.ArgumentException("message", nameof(value));
+            }
+            else
+            {
+                Value = Convert.ToInt32(value);
+            }
+        }
+    }
+}
