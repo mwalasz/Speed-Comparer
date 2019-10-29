2937e500d6242d2f190fd71962e3a8da5e45a444
mwalasz <mwalasz@interia.pl>
2019-10-29 22:33:23 +0100

    Dodanie metod rozszerzających dla konwersji na bajty

---
+++ MainProgram/Threads/ThreadsHandler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainProgram.Threads
+{
+    public class ThreadsHandler
+    {
+        
+        public void CreateThreads()
+        {
+
+        }
+
+        public void RunThreads()
+        {
+
+        }
+
+        public void WaitForThreads()
+        {
+
+        }
+    }
+}
