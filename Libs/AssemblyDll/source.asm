0f1993d71a28a663653ec0a054bde4bdeb4eb324
mwalasz <mwalasz@interia.pl>
2019-12-07 18:00:27 +0100

    Zmiany w asemblerze, ustawienie asemblera jako tymczasowej funckji startowej

--- Libs/AssemblyDll/source.asm
+++ Libs/AssemblyDll/source.asm
@@ -17,10 +17,10 @@ DllEntry ENDP
 MatrixScalarMultiplication PROC matrix: DWORD, scalar: REAL4, len: DWORD
 ;vbroadcastss xmm1, xmm2
 
-local skalar: REAL4
+local skalar: DWORD
 
-mov edx, [scalar]
-mov ebx, matrix ; EBX = ADRES TABLICY
+movss xmm7, xmm0
+mov ebx, skalar
 mov eax, [ebx + 1]
 mov eax, [ebx + 2]
 mov eax, [ebx + 3]
