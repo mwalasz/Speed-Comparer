2403d5d2e65236000537a538de6567bfca8eccf5
mwalasz <mwalasz@interia.pl>
2019-12-09 19:49:07 +0100

    Wczytywanie i jakieś operacje w asm

--- Libs/AssemblyDll/source.asm
+++ Libs/AssemblyDll/source.asm
@@ -9,7 +9,12 @@ DllEntry endp
 ; Arguments passed to function:
 ; RCX - pointer to array of floats
 ; RDX - scalar number
-; R8 - length of array of floats
+; R8  - length of array of floats
+;----------------------------------------------------------------------
+; Used registers in function:
+; RSI - array
+; RAX - scalar number
+; RBX - array length
 ;----------------------------------------------------------------------
 
 MatrixScalarMultiplication proc
@@ -17,7 +22,18 @@ MatrixScalarMultiplication proc
 	push RSI
 	push RAX
 	push RBX
-	jmp endProgram
+
+;load data
+	mov RSI, RCX ;load array
+	mov RAX, RDX ;load scalar
+	mov RBX, R8  ;load array length
+	vmovq XMM7, RAX ;load scalar to xmm7
+
+	movups xmm0, [rsi] ;za³adowanie 4 floatow do rejestru
+
+	vbroadcastss xmm1, xmm7 ;za³adowanie do kazdej z komorek xmm1 skalara
+	vmulps xmm0, xmm1, xmm0 ;mnozenie xmm0 * xmm1 i zapisanie do xmm0
+	movups [rsi], xmm0
 
 ;restoring registers to original state from before function execution
 endProgram:
